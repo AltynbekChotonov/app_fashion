25e0bd70effcd0a8cd090a77e8d67241e9e8b50d
AltynbekChotonov <dtvkanalnews@gmail.com>
2023-04-12 17:24:48 +0600

    app жасадым

---
+++ android/app/src/main/kotlin/com/example/app_fashion/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_fashion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: RichText(
+            text: TextSpan(
+                style: TextStyle(color: Colors.red),
+                children: <TextSpan>[
+              TextSpan(text: "I   "),
+              TextSpan(
+                  text: "applications", style: TextStyle(color: Colors.yellow))
+            ])),
+        actions: [
+          Icon(
+            Icons.facebook,
+            color: Colors.blue,
+            size: 35,
+          ),
+          SizedBox(width: 10),
+          Padding(
+            padding: const EdgeInsets.only(right: 35),
+            child: Icon(
+              Icons.favorite,
+              color: Colors.red,
+              size: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
