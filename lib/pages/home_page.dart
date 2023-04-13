2109199f1e45ae2bb0d34cd794af4db48be92966
AltynbekChotonov <dtvkanalnews@gmail.com>
2023-04-13 22:02:20 +0600

    AppBar icons

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,35 +6,45 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        title: RichText(
-            text: TextSpan(
-                style: TextStyle(color: Colors.red),
-                children: <TextSpan>[
-              TextSpan(text: "I   "),
-              TextSpan(
-                  text: "applications", style: TextStyle(color: Colors.yellow))
-            ])),
-        actions: [
-          Icon(
-            Icons.facebook,
-            color: Colors.blue,
-            size: 35,
-          ),
-          SizedBox(width: 10),
-          Padding(
-            padding: const EdgeInsets.only(right: 35),
-            child: Icon(
-              Icons.favorite,
-              color: Colors.red,
-              size: 20,
-            ),
-          ),
-        ],
-      ),
-    );
+        appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.white,
+            centerTitle: true,
+            title: RichText(
+                text: TextSpan(
+                    style: TextStyle(color: Colors.red),
+                    children: <TextSpan>[
+                  TextSpan(text: "I   "),
+                  TextSpan(
+                      text: "applications",
+                      style: TextStyle(color: Colors.yellow))
+                ])),
+            actions: [
+              Icon(
+                Icons.facebook,
+                color: Colors.blue,
+                size: 35,
+              ),
+              SizedBox(width: 10),
+              Padding(
+                padding: const EdgeInsets.only(right: 35),
+                child: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                  size: 20,
+                ),
+              ),
+            ],
+            leading: Row(
+              children: [
+                Icon(Icons.add, color: Colors.blueAccent),
+                Icon(Icons.mail, color: Colors.blueAccent),
+                Icon(
+                  Icons.face,
+                  color: Colors.blueAccent,
+                  size: 5,
+                ),
+              ],
+            )));
   }
 }
