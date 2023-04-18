e15628e2f631d4e6adbdea72f0edfae5c24587bc
AltynbekChotonov <dtvkanalnews@gmail.com>
2023-04-18 16:55:40 +0600

    body text jasoo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,55 +6,75 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            flexibleSpace: Image.network(
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ9dz3a-yebsM1VP6JhDCnxCvvg1D7mDs1Qg&usqp=CAU'),
-            toolbarHeight: 100,
-            elevation: 5,
-            backgroundColor: Colors.white,
-            centerTitle: true,
-            title: RichText(
-                text: TextSpan(
-                    style: TextStyle(color: Colors.red),
-                    children: <TextSpan>[
-                  TextSpan(text: "I   "),
-                  TextSpan(
-                      text: "applications",
-                      style: TextStyle(color: Colors.yellow))
-                ])),
-            actions: [
-              PopupMenuButton(itemBuilder: (BuildContext context) {
-                return [
-                  PopupMenuItem(child: Text("O tirkeme")),
-                  PopupMenuItem(child: Text("Men jonundo")),
-                  PopupMenuItem(child: Text("talruu")),
-                ];
-              }),
-              Icon(
-                Icons.facebook,
-                color: Colors.blue,
-                size: 10,
-              ),
-              SizedBox(width: 10),
-              Padding(
-                padding: const EdgeInsets.only(right: 35),
-                child: Icon(
-                  Icons.favorite,
-                  color: Colors.red,
-                  size: 10,
+      appBar: AppBar(
+        flexibleSpace: Image.network(
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ9dz3a-yebsM1VP6JhDCnxCvvg1D7mDs1Qg&usqp=CAU'),
+        toolbarHeight: 100,
+        elevation: 5,
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: RichText(
+            text: const TextSpan(
+                style: TextStyle(color: Colors.red),
+                children: <TextSpan>[
+              TextSpan(text: "I   "),
+              TextSpan(
+                  text: "applications", style: TextStyle(color: Colors.yellow))
+            ])),
+        actions: [
+          PopupMenuButton(itemBuilder: (BuildContext context) {
+            return [
+              const PopupMenuItem(child: Text("O tirkeme")),
+              const PopupMenuItem(child: Text("Men jonundo")),
+              const PopupMenuItem(child: Text("talruu")),
+            ];
+          }),
+          const Icon(
+            Icons.facebook,
+            color: Colors.blue,
+            size: 10,
+          ),
+          const SizedBox(width: 10),
+          const Padding(
+            padding: EdgeInsets.only(right: 35),
+            child: Icon(
+              Icons.favorite,
+              color: Colors.red,
+              size: 10,
+            ),
+          ),
+        ],
+        leading: Row(
+          children: const [
+            Icon(Icons.add, color: Colors.blueAccent, size: 10),
+            Icon(Icons.mail, color: Colors.blueAccent, size: 10),
+            Icon(
+              Icons.face,
+              color: Colors.blueAccent,
+              size: 10,
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              color: Colors.black,
+              height: 120,
+              width: 80,
+              child: Center(
+                child: Text(
+                  'Flutter camp',
+                  //textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white),
                 ),
               ),
-            ],
-            leading: Row(
-              children: [
-                Icon(Icons.add, color: Colors.blueAccent, size: 10),
-                Icon(Icons.mail, color: Colors.blueAccent, size: 10),
-                Icon(
-                  Icons.face,
-                  color: Colors.blueAccent,
-                  size: 10,
-                ),
-              ],
-            )));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
