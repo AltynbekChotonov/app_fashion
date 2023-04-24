e054a2e8b8874f4e5cbd496443f6fc42c91028e5
AltynbekChotonov <dtvkanalnews@gmail.com>
2023-04-24 15:31:42 +0600

    button jasoo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -31,18 +31,98 @@ class HomePage extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 300),
-              child: appName,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 150),
-              child: badyName,
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 100),
+              appName,
+              SizedBox(height: 290),
+              badyName,
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Container(
+                  color: Color.fromARGB(64, 117, 134, 156),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      isCollapsed: false,
+                      hintText: 'Enter username',
+                      hintStyle: TextStyle(
+                        color: Color(0xFF99FFFFFF),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                      border: OutlineInputBorder(),
+                      //labelText: 'Enter username',
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Container(
+                  color: Color.fromARGB(64, 117, 134, 156),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      suffixIcon: const Icon(
+                        Icons.visibility,
+                        color: Color(0xFFFAFAFA),
+                      ),
+
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                      border: OutlineInputBorder(),
+                      //labelText: 'Enter username',
+                      hintText: 'Password',
+                      hintStyle: TextStyle(
+                        color: Color(0xFF99FFFFFF),
+                      ),
+                    ),
+                    obscureText: true,
+                  ),
+                ),
+              ),
+              SizedBox(height: 5),
+              const Padding(
+                padding: EdgeInsets.only(left: 250),
+                child: Text(
+                  'Forgot Password?',
+                  style: TextStyle(color: Color(0xFF75869C)),
+                ),
+              ),
+              const SizedBox(height: 5),
+              Container(
+                height: 50,
+                width: 350,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(35),
+                  color: Colors.blue,
+                ),
+                child: const Center(
+                    child: Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white, fontSize: 19),
+                )),
+              ),
+              const SizedBox(height: 10),
+              RichText(
+                text: const TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'Dont have an Account?',
+                      style: TextStyle(color: Color(0xFF75869C), fontSize: 17),
+                    ),
+                    TextSpan(
+                      text: ' Registration',
+                      style: TextStyle(color: Color(0xFF1872F6), fontSize: 17),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     ));
